89350ef5dfb4c02a1a973e58337e9ffe179006a1
wzjchen <waleyzjchen@gmail.com>
2013-11-01 08:44:27 -0700

    Moved some classes.

---
+++ src/commits/CommitHistoryController.java
@@ -0,0 +1,10 @@
+package commits;
+
+/* placeholder class for the commit history controller/iterator
+  This class is supposed to iterate through a list of commits,
+  and for each one, revert the codebase to version of when the
+  commit was made, and feed it to the frame generator block.
+*/
+public class CommitHistoryController {
+	// todo
+}
