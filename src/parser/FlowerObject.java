f73f8b5d7355422729fa9170ea6478c24c0a4e5b
majinknight <dumajin@hotmail.com>
2013-11-01 08:34:47 -0700

    adding more fields to flowerobject, deleting dummy target class

--- src/parser/FlowerObject.java
+++ src/parser/FlowerObject.java
@@ -11,9 +11,22 @@ public class FlowerObject {
 	private String name;
 	private String packname;
 	private ArrayList<String> importClasses = new ArrayList<String>();
+	private double xCoord;
+	private double yCoord;
 	
 	
-	
+	public double getxCoord() {
+		return xCoord;
+	}
+	public void setxCoord(double xCoord) {
+		this.xCoord = xCoord;
+	}
+	public double getyCoord() {
+		return yCoord;
+	}
+	public void setyCoord(double yCoord) {
+		this.yCoord = yCoord;
+	}
 	public String getName() {
 		return name;
 	}
